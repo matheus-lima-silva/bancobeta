54a70ef3fd4dd3d58c306674ddaf9ad950cc5b46
matheus-lima-silva <150077612+matheus-lima-silva@users.noreply.github.com>
2024-06-15 00:01:06 -0300

    fix: erro inicializacao

--- src/main/java/com/bancobeta/bancobeta/config/DatabaseInitializer.java
+++ src/main/java/com/bancobeta/bancobeta/config/DatabaseInitializer.java
@@ -1,21 +1,19 @@
 package com.bancobeta.bancobeta.config;
 
-
 import com.bancobeta.bancobeta.model.entity.Cliente;
+import com.bancobeta.bancobeta.model.entity.Conta;
 import com.bancobeta.bancobeta.model.entity.ContaCorrente;
 import com.bancobeta.bancobeta.model.entity.ContaPagamento;
 import com.bancobeta.bancobeta.repository.ClienteRepository;
 import com.bancobeta.bancobeta.repository.ContaRepository;
-
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Arrays;
 
-@Configuration
+@Component
 public class DatabaseInitializer implements CommandLineRunner {
-
 
     private final ClienteRepository clienteRepository;
     private final ContaRepository contaRepository;
@@ -26,6 +24,7 @@ public class DatabaseInitializer implements CommandLineRunner {
     }
 
     @Override
+    @Transactional
     public void run(String... args) {
         if (clienteRepository.count() == 0) {
             Cliente cliente1 = new Cliente("João Silva", "17163934777", "joao@email.com", "Rua A, 123", 5000.0);
@@ -33,10 +32,11 @@ public class DatabaseInitializer implements CommandLineRunner {
 
             clienteRepository.saveAll(Arrays.asList(cliente1, cliente2));
 
-            ContaCorrente contaCorrente1 = new ContaCorrente("12345-6", 1000.0, cliente1, 500.0);
-            ContaPagamento contaPagamento2 = new ContaPagamento("98765-4", 500.0, cliente2);
+            Conta contaCorrente1 = new ContaCorrente("12345-6", 1000.0, cliente1, 500.0);
+            Conta contaPagamento2 = new ContaPagamento("98765-4", 500.0, cliente2);
 
             contaRepository.saveAll(Arrays.asList(contaCorrente1, contaPagamento2));
         }
     }
 }
+
